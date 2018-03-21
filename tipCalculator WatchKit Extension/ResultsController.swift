aa6d5530e2bad92c08d41b796938e2a07403a1c6
William Hettinger <bill.hettinger@gmail.com>
2018-03-21 18:00:38 +0100

    visual interface done, two interface Controllers

---
+++ tipCalculator WatchKit Extension/ResultsController.swift
@@ -0,0 +1,14 @@
+//
+//  ResultsController.swift
+//  tipCalculator WatchKit Extension
+//
+//  Created by William Hettinger on 3/21/18.
+//  Copyright © 2018 William Hettinger. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class ResultsController: WKInterfaceController {
+
+}
